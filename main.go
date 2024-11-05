bd68ec4c7720e53a68ca431813dd7fd4224de56b
Hunainsiddiqui <hunainsiddiqui93@gmail.com>
2024-11-05 16:35:24 +0530

    cors

--- main.go
+++ main.go
@@ -9,9 +9,28 @@ import (
 func main() {
 	r := router.SetupRouter()
 
+	r.Use(CORSMiddleware)
+
 	log.Println("Server running on :3000")
 	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		log.Fatal("ListenAndServe error:", err)
 	}
+}
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
